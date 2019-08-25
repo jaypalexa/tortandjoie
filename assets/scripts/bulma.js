b107d517b3dfb5ca18b23579c98961dee782375a
jaypalexa <jpalexa@gmail.com>
2019-08-25 14:20:02 +0100

    Removed nav; all content on main page

--- assets/scripts/bulma.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const $navbarBurger = document.querySelector('.navbar-burger');
-  $navbarBurger.addEventListener('click', () => {
-    const target = $navbarBurger.dataset.target;
-    const $target = document.getElementById(target);
-    $navbarBurger.classList.toggle('is-active');
-    $target.classList.toggle('is-active');
-  });
-});
